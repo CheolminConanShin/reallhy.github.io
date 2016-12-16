cdfad3176b0caba4fed41307105f7d67902333f4
devcraft17 <devcraft17@devcraft17s-iMac.local>
2016-12-16 09:13:39 +0900

    select route button shows one route for each

--- src/javascript/route_select.js
+++ src/javascript/route_select.js
@@ -1,7 +1,8 @@
 var map, directionsService;
 var priorityType;
 // 초기화 함수
-function init() {
+var documentReady = function() {
+	directionsService = new olleh.maps.DirectionsService('frKMcOKXS*l9iO5g');
 	let geolocation_callback = function(position) {
 		map = new olleh.maps.Map('map_div', {
 			center : new olleh.maps.LatLng(position.coords.latitude, position.coords.longitude),
@@ -13,67 +14,54 @@ function init() {
 			scaleControl: false,
 			panControl: false,
 		});
-
 		// sds : 37.516580, 127.100821
 		// 수원 : 37.278013, 127.034959
 		// origin : new olleh.maps.UTMK(960487, 1955309.75), 
 		// destination : new olleh.maps.UTMK(960804.5, 1956454), 
-		
-		directionsService = new olleh.maps.DirectionsService('frKMcOKXS*l9iO5g');
-		directionsService.route({
-				origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
-				destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
-				projection : olleh.maps.DirectionsProjection.UTM_K, 
-				travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
-				priority : olleh.maps.DirectionsDrivePriority.PRIORITY_0
-			}, 
-			getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_0)
-		); 
-		directionsService.route({
-				origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
-				destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
-				projection : olleh.maps.DirectionsProjection.UTM_K, 
-				travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
-				priority : olleh.maps.DirectionsDrivePriority.PRIORITY_1
-			}, 
-			getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_1)
-		); 
-		directionsService.route({
-				origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
-				destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
-				projection : olleh.maps.DirectionsProjection.UTM_K, 
-				travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
-				priority : olleh.maps.DirectionsDrivePriority.PRIORITY_2
-			}, 
-			getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_2)
-		); 
-		directionsService.route({
-				origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
-				destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
-				projection : olleh.maps.DirectionsProjection.UTM_K, 
-				travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
-				priority : olleh.maps.DirectionsDrivePriority.PRIORITY_3
-			}, 
-			getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_3)
-		); 
 	}
 	navigator.geolocation.getCurrentPosition(geolocation_callback, null, null);
 }
 
-$(document).ready(function() {
-	init();
-	var flag = true;
-	$('html').on("click", function(){
-		if(flag) {
-			var screen = document.querySelector('html');
-			if(screen.requestFullScreen) {
-			    screen.requestFullScreen();
-			} else if(screen.mozRequestFullScreen) {
-				screen.mozRequestFullScreen();
-			} else if(screen.webkitRequestFullScreen) {
-				screen.webkitRequestFullScreen();
-			}
-		}
-		flag = false;
-	});	
-});
+var clearMap = function() {
+	let polylines = document.querySelectorAll('#layer_container svg polyline')
+	for (var polyline of polylines) {
+		polyline.remove();
+	}
+}
+
+var recommendedRoute = function() {
+	clearMap();
+	directionsService.route({
+		origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
+		destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
+		projection : olleh.maps.DirectionsProjection.UTM_K, 
+		travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
+		priority : olleh.maps.DirectionsDrivePriority.PRIORITY_0
+	}, 
+	getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_0)
+	); 
+}	
+var shortestRoute = function() {
+	clearMap();
+	directionsService.route({
+		origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
+		destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
+		projection : olleh.maps.DirectionsProjection.UTM_K, 
+		travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
+		priority : olleh.maps.DirectionsDrivePriority.PRIORITY_1
+	}, 
+	getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_1)
+	); 
+}
+var freeRoute = function() {
+	clearMap();
+	directionsService.route({
+		origin : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.516580, 127.100821)),
+		destination : new olleh.maps.UTMK.valueOf(new olleh.maps.LatLng(37.278013, 127.034959)),
+		projection : olleh.maps.DirectionsProjection.UTM_K, 
+		travelMode : olleh.maps.DirectionsTravelMode.DRIVING,
+		priority : olleh.maps.DirectionsDrivePriority.PRIORITY_2
+	}, 
+	getCallbackString(olleh.maps.DirectionsDrivePriority.PRIORITY_2)
+	); 
+}
